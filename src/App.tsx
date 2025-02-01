cee2c210163e0f5856372b023943914b2c820541
Katherine Reed <katherine.grace.reed@gmail.com>
2025-02-01 10:39:04 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+export default function App() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <>
+      <header>
+        <h1>Kat's Social Media App</h1>
+      </header>
+      <main>
+        <h2>Work in progress...</h2>
+        <p>Please be patient</p>
+      </main>
+      <footer>
+        <small>copyright &copy; {currentYear} Reed Development</small>
+      </footer>
+    </>
+  )
+}
